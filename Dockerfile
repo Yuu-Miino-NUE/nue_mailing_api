f3201e4bd3edb210bcbedec5216a342a860d40a9
Yuu Miino <ymiino@naruto-u.ac.jp>
2025-06-03 14:25:31 +0900

    Finalize

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.13-alpine
+WORKDIR /app
+COPY src/ .
+RUN pip install --no-cache-dir -r requirements.txt
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--workers", "4"]
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import APIKeyHeader
 from pydantic import BaseModel, EmailStr
 from email.mime.text import MIMEText
 from email.utils import formataddr
@@ -10,22 +11,30 @@ SMTP_PORT = 25
 
 with open("apikeys.json", "r") as f:
     API_KEYS = {entry["key"]: entry["label"] for entry in json.load(f)}
+APY_KEY_NAME = "X-API-KEY"
+
+api_key_header = APIKeyHeader(name=APY_KEY_NAME, auto_error=False)
+
+
+async def evaluate_api_key(api_key_header: str = Depends(api_key_header)):
+    if api_key_header in API_KEYS:
+        return True
+    raise HTTPException(status_code=401, detail="Invalid API key")
+
 
 app = FastAPI()
+
 
 class EmailPayload(BaseModel):
     to: EmailStr
     subject: str
     body: str
     from_address: EmailStr
-    from_name: str  # ← 表示名
+    from_name: str
+
 
 @app.post("/send")
-def send_email(payload: EmailPayload, request: Request):
-    api_key = request.headers.get("X-API-KEY")
-    if api_key not in API_KEYS:
-        raise HTTPException(status_code=401, detail="Invalid API key")
-
+def send_email(payload: EmailPayload, _: bool = Depends(evaluate_api_key)):
     msg = MIMEText(payload.body)
     msg["Subject"] = payload.subject
     msg["From"] = formataddr((payload.from_name, payload.from_address))
@@ -35,6 +44,8 @@ def send_email(payload: EmailPayload, request: Request):
         with smtplib.SMTP(SMTP_HOST, SMTP_PORT, timeout=10) as smtp:
             smtp.send_message(msg)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"SMTP送信失敗: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Failed to send email via SMTP: {e}"
+        )
 
-    return {"message": "メールを送信しました"}
+    return {"message": "successfully sent"}
